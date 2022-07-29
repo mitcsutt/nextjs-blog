0366a890b74d50be634a78af333da5b36c5d7e84
Mitchell Sutton <mitchell.sutton@activepipe.com>
2022-07-29 16:17:04 +1000

    Learned routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,44 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout/Layout';
+import Layout from '../components/layout/Layout';
 import utilStyles from '../styles/utils.module.scss';
+import { getSortedPostsData, Post } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date/Date';
 
-export default function Home() {
+interface HomeProps {
+  allPostsData: Post[]
+}
+
+export default function Home({ allPostsData }: HomeProps) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, I am Mitchell. I am an experienced React software developer at MoxiWorks, but this is my first time using Next.js </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
